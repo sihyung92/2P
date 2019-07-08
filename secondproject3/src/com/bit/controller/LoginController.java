da4ef3abff420b79d71d0b5356b1aff2b4236e68
suyyy <suyeon0911@naver.com>
2019-07-08 14:52:13 +0900

    11

--- secondproject3/src/com/bit/controller/LoginController.java
+++ secondproject3/src/com/bit/controller/LoginController.java
@@ -13,27 +13,31 @@ import javax.servlet.http.HttpSession;
 import com.bit.model.UserDao;
 import com.bit.model.UserDto;
 
-@WebServlet("/lms/intro.bit")
+@WebServlet("/LMS/intro.bit")
 public class LoginController extends HttpServlet{
-	
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
 		System.out.println("LoginController / method : get");
-		RequestDispatcher rd = req.getRequestDispatcher("/lms/intro.jsp");
+		RequestDispatcher rd = req.getRequestDispatcher("/LMS/intro.jsp");
 		//받는 곳에서 request.getAttribute("loginWrong")!=null 이면 out.println(request.getAttribute(loginWrong))시켜야함
 		//session.getAttribute("kind")에 따라 jquery로 제어
 		rd.forward(req, resp);
 	}
-	
+
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
 		System.out.println("LoginController / method : post");
 		UserDao dao = new UserDao();
-		UserDto bean = dao.login(req.getParameter("id"),req.getParameter("pw"));
-		if(bean==null) {
-			req.setAttribute("loginWrong", "<script type=\"text/javascript\">alert('아이디 혹은 비밀번호를 다시 확인해주세요');</script>");
-			//받는 곳에서 request.getAttribute(loginWrong)!=null 이면 out.println(request.getAttribute(loginWrong))시켜야함
-		}else {
+		UserDto bean = dao
+				.login(req.getParameter("id"), req.getParameter("pw"));
+		if (bean == null) {
+			req.setAttribute("loginWrong",
+					"<script type=\"text/javascript\">alert('아이디 혹은 비밀번호를 다시 확인해주세요');</script>");
+			// 받는 곳에서 request.getAttribute(loginWrong)!=null 이면
+			// out.println(request.getAttribute(loginWrong))시켜야함
+		} else {
 			HttpSession session = req.getSession();
 			session.setAttribute("isLogin", true);
 			session.setAttribute("id", bean.getId());
@@ -41,6 +45,6 @@ public class LoginController extends HttpServlet{
 			session.setAttribute("userNum", bean.getUserNum());
 			session.setAttribute("kind", bean.getKind());// 0학생 1강사 2관리자
 		}
-		doGet(req,resp);
+		doGet(req, resp);
 	}
 }
