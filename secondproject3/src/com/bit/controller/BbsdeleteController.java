29eb1a4c74141ee3f83c363c0f5ca43761643af9
minmin19 <mycjswo113@gmail.com>
2019-07-08 18:11:20 +0900

    board

---
+++ secondproject3/src/com/bit/controller/BbsdeleteController.java
@@ -0,0 +1,24 @@
+package com.bit.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bit.model.BbsDao;
+
+public class BbsdeleteController extends HttpServlet{
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		int num=Integer.parseInt(req.getParameter("num"));
+		int bbsnum=Integer.parseInt(req.getParameter("bbsnum"));
+		
+		BbsDao dao=new BbsDao();
+		dao.delete(num,bbsnum);
+		resp.sendRedirect("BbsquestuList.jsp");
+	}
+}
